9c7007deb886c33005b9f88ce4e664dd016a35b1
kevin <kevin@kevin-G75VW.(none)>
2013-03-14 22:28:57 -0400

    Base Rails Install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssReader::Application.config.secret_token = '096aee3be6c3674aeb2cf2306f504d75cb1c99cb307394ecd433a4fb2ab495c4236b538f1884e6a59f254a9b9010536acc2ed5f8c5acce0bea73f651fb54f962'
